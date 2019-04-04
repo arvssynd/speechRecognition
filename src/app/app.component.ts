c9ce339b3261988b88f24f4c00116ac0be65e5b7
Giacomo <giacomo.caldironi@vecompsoftware.it>
2019-04-04 11:27:04 +0200

    before material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,40 +35,4 @@ export class AppComponent implements OnInit {
   cerca() {
     alert('cerca');
   }
-
-
-  testvoce() {
-    // this.creaAccessToken().subscribe(data => {
-    //   const builder = require('xmlbuilder');
-
-    //   console.log(data);
-    //   const xml_body = builder.create('speak')
-    //     .att('version', '1.0')
-    //     .att('xml:lang', 'en-us')
-    //     .ele('voice')
-    //     .att('xml:lang', 'en-us')
-    //     .att('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
-    //     .txt('select balance')
-    //     .end();
-
-    //   const body = xml_body.toString();
-    //   const _headers: HttpHeaders = new HttpHeaders()
-    //     .append('Content-Type', 'application/ssml+xml')
-    //     .append('X-Microsoft-OutputFormat', 'riff-24khz-16bit-mono-pcm')
-    //     .append('cache-control', 'no-cache')
-    //     .append('Authorization', 'Bearer ' + data);
-
-    //   this._http.post('https://westeurope.tts.speech.microsoft.com/cognitiveservices/v1', body, { headers: _headers, responseType: 'blob' })
-    //     .subscribe(blob => {
-    //       const url = URL.createObjectURL(blob);
-    //       // Get DOM elements.
-    //       const audio = new Audio();
-    //       audio.src = url;
-    //       audio.load();
-    //       audio.play().catch(function (error) {
-    //         console.log(error.message);
-    //       });
-    //     });
-    // });
-  }
 }
